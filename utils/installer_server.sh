9e671001422d84b4b6d31c46c29892a2150668d9
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2013-05-04 22:32:46 +0200

    hotfix

--- utils/installer_server.sh
+++ utils/installer_server.sh
@@ -6,4 +6,5 @@ then
 else
     cd ~ ; git clone https://github.com/andrzejsliwa/vimfiles.git ~/.vim-andrzej ; cd -
     grep "alias avim" ~/.bashrc >/dev/null  || echo 'alias avim="vim -u ~/.vim-andrzej/vimrc"' >> ~/.bashrc
+    source ~/.bashrc
 fi
