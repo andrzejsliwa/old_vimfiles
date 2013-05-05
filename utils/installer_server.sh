7f771735bf0d4aa5d0a850d3c7d197d08075756f
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2013-05-05 12:32:47 +0200

    testing install scripts

--- utils/installer_server.sh
+++ utils/installer_server.sh
@@ -4,6 +4,7 @@ if [ -d $HOME/.vim-andrzej ]
 then
     cd ~/.vim-andrzej ; git pull ; cd -
 else
-    cd ~ ; git clone https://github.com/andrzejsliwa/vimfiles.git ~/.vim-andrzej ; cd -
+    git clone https://github.com/andrzejsliwa/vimfiles.git ~/.vim-andrzej
+    git clone https://github.com/gmarik/vundle.git ~/.vim-andrzej/bundle/vundle
     grep "alias avim" ~/.bashrc >/dev/null  || echo 'alias avim="vim -u ~/.vim-andrzej/vimrc"' >> ~/.bashrc
 fi
